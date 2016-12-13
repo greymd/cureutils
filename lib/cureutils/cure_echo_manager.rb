5ec9afd020193d011c4ce38f9d594767cd328c91
greymd <greengregson@gmail.com>
2016-12-13 22:47:38 +0900

    cure echo command supports Maho girls precure.

--- lib/cureutils/cure_echo_manager.rb
+++ lib/cureutils/cure_echo_manager.rb
@@ -13,6 +13,7 @@ class CureEchoManager
     @err = $stderr
     @cure_name = 'echo'
     @message_mode = EchoMode::TRANSFORM
+    @style_priority = [:diamond, :emerald]
   end
 
   def source_output(source = $stdout)
@@ -46,16 +47,33 @@ class CureEchoManager
 
   def print_results
     return 1 unless existing_precure?
+    precure = Cure.send(@cure_name.to_sym)
     if @message_mode == EchoMode::TRANSFORM
-      Cure.send(@cure_name.to_sym).transform!
+      precure = original_transform(precure)
     elsif @message_mode == EchoMode::ATTACK
-      Cure.send(@cure_name.to_sym).transform!
-      Cure.send(@cure_name.to_sym).attack!
+      precure = original_transform(precure)
+      precure.attack!
     end
     0
   end
 
   private
+
+  def original_transform (precure)
+    if precure.respond_to?(:transform_styles)
+      chosen_style = @style_priority.find do | s |
+        precure.transform_styles.include?(s)
+      end
+      if chosen_style
+        precure.transform! chosen_style
+      else
+        default_style, _entity = precure.transform_styles.first
+        precure.transform! default_style
+      end
+    else
+      precure.transform!
+    end
+  end
 
   def existing_precure?
     found_precure = Rubicure::Girl.config.find do |k, _v|
