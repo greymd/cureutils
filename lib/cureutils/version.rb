93104ac360b937403758644bde0d9e7be4d45d79
greymd <greengregson@gmail.com>
2017-03-26 22:38:26 +0900

    Bump version to 1.0.0

--- lib/cureutils/version.rb
+++ lib/cureutils/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 module Cureutils
   class Version
-    MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
     PRE = nil
 
     class << self
